7b8a55edef8a8df259dab27b1619424c13521e2a
Keep Silent <qatheorys@outlook.com>
2020-11-12 18:01:50 +0700

    update batch create vias

--- src/pages/Via/CreateVia/CreateVia.js
+++ src/pages/Via/CreateVia/CreateVia.js
@@ -1,5 +1,6 @@
 import React from "react";
 import clsx from "clsx";
+import * as XLSX from "xlsx";
 import { makeStyles, theme } from "@material-ui/core/styles";
 import { useSelector, useDispatch } from "react-redux";
 import {
@@ -12,15 +13,44 @@ import {
 	Checkbox,
 	Button,
 	CircularProgress,
+	Dialog,
+	DialogActions,
+	DialogContent,
+	DialogContentText,
+	DialogTitle,
+	Table,
+	TableContainer,
+	TableHead,
+	TableRow,
+	TableCell,
+	TableBody,
 } from "@material-ui/core";
 import blue from "@material-ui/core/colors/blue";
 
 import { useSnackbar } from "notistack";
-
+import CheckCircleOutlineIcon from "@material-ui/icons/CheckCircleOutline";
+import ErrorOutlineIcon from "@material-ui/icons/ErrorOutline";
+import AddCircleOutlineOutlinedIcon from "@material-ui/icons/AddCircleOutlineOutlined";
+import PauseCircleOutlineIcon from "@material-ui/icons/PauseCircleOutline";
 import axios from "axios";
 import Constants from "_helpers/localConstants.js";
 import Commons from "_helpers/commons.js";
-
+let defaultFormState = {
+	formValues: {
+		viaName: "",
+		viaFbid: "",
+		viaEmail: "",
+		viaPassword: "",
+		viaEmailPassword: "",
+		viaAccessToken: "",
+		viaTFA: "",
+		viaFbName: "",
+		viaFbLink: "",
+		viaGender: undefined,
+		viaDob: undefined,
+		viaLabel: "",
+	},
+};
 const useStyles = makeStyles((theme) => ({
 	root: {
 		padding: "0px",
@@ -70,26 +100,23 @@ const useStyles = makeStyles((theme) => ({
 		backgroundColor: "rgba(0,0,0,0.05)",
 	},
 }));
+const columns = [
+	{ id: "name", label: "VIA", minWidth: 100 },
+	{ id: "note", label: "Ghi chú", minWidth: 170 },
+	{
+		id: "status",
+		label: "Trạng thái",
+		minWidth: 100,
+	},
+];
 function CreateVIA(props) {
 	const classes = useStyles();
 	const { enqueueSnackbar, closeSnackbar } = useSnackbar();
 	const [loading, setLoading] = React.useState(false);
-	const [formState, setFormState] = React.useState({
-		formValues: {
-			viaName: "",
-			viaFbid: "",
-			viaEmail: "",
-			viaPassword: "",
-			viaEmailPassword: "",
-			viaAccessToken: "",
-			viaTFA: "",
-			viaFbName: "",
-			viaFbLink: "",
-			viaGender: undefined,
-			viaDob: undefined,
-			viaLabel: "",
-		},
-	});
+	const [batchUploadStatus, setBatchUploadStatus] = React.useState(false);
+	const [queueId, setQueueId] = React.useState(false);
+	const [batchUploadVias, setBatchUploadVias] = React.useState([]);
+	const [formState, setFormState] = React.useState(defaultFormState);
 	const handleChange = ({ target }) => {
 		const { formValues } = formState;
 		formValues[target.name] = target.value;
@@ -99,7 +126,7 @@ function CreateVIA(props) {
 	const handleCancel = () => {
 		props.history.push("/admin/manage-via");
 	};
-	const createVia = () => {
+	const createVia = async (event, queueId) => {
 		const { formValues } = formState;
 		let header = Commons.header();
 		let data = {
@@ -129,16 +156,35 @@ function CreateVIA(props) {
 		})
 			.then((resp) => {
 				setLoading(false);
+				if (queueId) {
+					let newBatchUploadVias = [...batchUploadVias];
+					if (resp.data.status == "updated") {
+						newBatchUploadVias.note = "cập nhật thành công";
+						newBatchUploadVias.status = "updated";
+					}
+					if (resp.data.status == "created") {
+						newBatchUploadVias.note = "Thêm via thành công";
+						newBatchUploadVias.status = "created";
+					}
+					return;
+				}
 				props.history.push("/admin/manage-via");
 				// console.log(resp.data);
 			})
 			.catch((err) => {
 				setLoading(false);
-				props.history.push("/admin/manage-via");
 				console.log(err);
+				if (queueId) {
+					let newBatchUploadVias = [...batchUploadVias];
+					newBatchUploadVias.note = "Đã xảy ra lỗi không xác định";
+					newBatchUploadVias.status = "error";
+					return;
+				}
+				props.history.push("/admin/manage-via");
 			});
 	};
-	const handleSubmit = () => {
+	const handleSubmit = async (event, queueId) => {
+		console.log(queueId);
 		const { formValues } = formState;
 		setLoading(true);
 		axios({
@@ -147,7 +193,7 @@ function CreateVIA(props) {
 			params: { access_token: formValues.viaAccessToken.trim() },
 		})
 			.then((resp) => {
-				createVia();
+				createVia(queueId);
 			})
 			.catch((err) => {
 				console.log(err.response.data);
@@ -156,13 +202,119 @@ function CreateVIA(props) {
 						variant: "error",
 					});
 					setLoading(false);
+					if (queueId) {
+						let newBatchUploadVias = [...batchUploadVias];
+						newBatchUploadVias.note = "Access token không hợp lệ";
+						newBatchUploadVias.status = "error";
+						return;
+					}
 					return;
 				}
 				enqueueSnackbar("Đã có lỗi xảy ra!!!", {
 					variant: "error",
 				});
 				setLoading(false);
+				if (queueId) {
+					let newBatchUploadVias = [...batchUploadVias];
+					newBatchUploadVias.note = "Xảy ra lỗi không xác định";
+					newBatchUploadVias.status = "error";
+					return;
+				}
+				return;
 			});
+	};
+	const convertToJson = (csv) => {
+		var lines = csv.split("\n");
+
+		var result = [];
+
+		var headers = lines[0].split(",");
+
+		for (var i = 1; i < lines.length; i++) {
+			var obj = {};
+			var currentline = lines[i].split(",");
+
+			for (var j = 0; j < headers.length; j++) {
+				obj[headers[j]] = currentline[j];
+			}
+			if (obj.via != "") {
+				result.push(obj);
+			}
+		}
+
+		//return result; //JavaScript object
+		return result; //JSON
+	};
+
+	// START Batch create section
+
+	const handleUploadFile = (event) => {
+		let file = event.target.files[0];
+		const reader = new FileReader();
+		reader.onload = (evt) => {
+			// evt = on_file_select event
+			/* Parse data */
+			const bstr = evt.target.result;
+			const wb = XLSX.read(bstr, { type: "binary" });
+			/* Get first worksheet */
+			const wsname = wb.SheetNames[0];
+			const ws = wb.Sheets[wsname];
+			/* Convert array of arrays */
+			const data = XLSX.utils.sheet_to_csv(ws, { header: 1 });
+			/* Update state */
+			let listCreateVias = convertToJson(data); // shows data in json format
+			listCreateVias = listCreateVias.map((via) => {
+				return {
+					name: via.via,
+					data: via,
+					note: "",
+					status: "waiting",
+				};
+			});
+			setBatchUploadVias(listCreateVias);
+			importVias(listCreateVias);
+		};
+		reader.readAsBinaryString(file);
+		reader.close();
+	};
+	const importVias = async (listCreateVias) => {
+		setBatchUploadStatus(true);
+		for (let i = 0; i < listCreateVias.length; i++) {
+			let viasCreateParams = {
+				viaName: listCreateVias.via,
+				viaFbid: listCreateVias.fbid,
+				viaEmail: listCreateVias.email,
+				viaPassword: listCreateVias.password,
+				viaEmailPassword: listCreateVias.email_password,
+				viaAccessToken: listCreateVias.access_token,
+				viaTFA: listCreateVias.tfa,
+			};
+
+			let newBatchUploadVias = [...batchUploadVias];
+			newBatchUploadVias[i].status = "process";
+			newBatchUploadVias[i].note = "Đang xử lý";
+			SubmitListCreateVias(viasCreateParams);
+		}
+		setFormState(defaultFormState);
+		setQueueId(false);
+	};
+	const handleCloseBatchUpload = () => {
+		setBatchUploadStatus(false);
+	};
+	// END Batch create section
+	const getStatusIcon = (status) => {
+		switch (status) {
+			case "created":
+				return <AddCircleOutlineOutlinedIcon />;
+			case "updated":
+				return <CheckCircleOutlineIcon />;
+			case "error":
+				return <ErrorOutlineIcon />;
+			case "waiting":
+				return <PauseCircleOutlineIcon />;
+			default:
+				return <CircularProgress />;
+		}
 	};
 	return (
 		<div>
@@ -337,6 +489,28 @@ function CreateVIA(props) {
 							>
 								Tạo VIA
 							</Button>
+
+							<input
+								accept=".csv"
+								className={classes.input}
+								style={{ display: "none" }}
+								id="raised-button-file"
+								multiple
+								type="file"
+								disabled={loading}
+								onChange={handleUploadFile}
+							/>
+							<label htmlFor="raised-button-file">
+								<Button
+									component="span"
+									variant="contained"
+									color="primary"
+									disabled={loading}
+									className={classes.button}
+								>
+									Upload
+								</Button>
+							</label>
 						</div>
 					</Paper>
 					{loading && (
@@ -345,6 +519,99 @@ function CreateVIA(props) {
 							className={classes.progress}
 						/>
 					)}
+					<Dialog
+						open={batchUploadStatus}
+						onClose={handleCloseBatchUpload}
+						aria-labelledby="form-dialog-title"
+					>
+						<DialogTitle id="form-dialog-title">
+							{`Đang tiến hành cập nhật Via`}
+						</DialogTitle>
+						<DialogContent>
+							<TableContainer>
+								<Table aria-label="sticky table">
+									<TableHead>
+										<TableRow>
+											{columns.map((column) => (
+												<TableCell
+													key={column.id}
+													align={column.align}
+													style={{
+														minWidth:
+															column.minWidth,
+													}}
+												>
+													{column.label}
+												</TableCell>
+											))}
+										</TableRow>
+									</TableHead>
+									<TableBody>
+										{batchUploadVias.slice().map((row) => {
+											return (
+												<TableRow
+													hover
+													role="checkbox"
+													tabIndex={-1}
+													key={row.id}
+												>
+													{columns.map(
+														(column, colIndex) => {
+															const value =
+																row[column.id];
+															if (
+																column.id ==
+																"status"
+															) {
+																return (
+																	<TableCell
+																		key={
+																			column.id
+																		}
+																		align={
+																			column.align
+																		}
+																	>
+																		{getStatusIcon(
+																			value
+																		)}
+																	</TableCell>
+																);
+															}
+															return (
+																<TableCell
+																	key={
+																		column.id
+																	}
+																	align={
+																		column.align
+																	}
+																>
+																	{column.format
+																		? column.format(
+																				value
+																		  )
+																		: value}
+																</TableCell>
+															);
+														}
+													)}
+												</TableRow>
+											);
+										})}
+									</TableBody>
+								</Table>
+							</TableContainer>
+						</DialogContent>
+						<DialogActions>
+							<Button
+								onClick={handleCloseBatchUpload}
+								color="primary"
+							>
+								Đóng
+							</Button>
+						</DialogActions>
+					</Dialog>
 				</div>
 			</Fade>
 		</div>
